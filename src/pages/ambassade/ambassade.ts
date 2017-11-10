df7d6c4366425b737e7f09965bd86499a9f713ae
Devarence <32297872+Devarence@users.noreply.github.com>
2017-11-10 16:37:57 +0000

    commit

--- src/pages/ambassade/ambassade.ts
+++ src/pages/ambassade/ambassade.ts
@@ -13,25 +13,80 @@ export class AmbassadePage {
   
   ambassade: Ambassade = new Ambassade();
 
+  selectedItem: any;
 
- /* pays: String[] = new Array<String>('Afriaue du sud', 'Ailgerie');
-  //ville_localisation: String = 'Abidjan';
-  adresse: String[] = new Array<String>('Cocody President 08 BP 1806 Abidjan 08', 
-    ' 53 Boulevard clozel 01 BP 1015 Abidjan 01');
-  telephone:String[] = new Array<String>('22 44 59 63', '20 32 32 40');
-  fax: String[] = new Array<String>('22 44 74 50', '20 22 37 12');
-  email: String[] = new Array<String>('abidjan@dirco.gov.za', 'aucun');
-  site_web: String[] = new Array<String>('aucun', 'aucun');
-  heure_ouverture: String[] = new Array<String>('Lundi-jeudi 08H-16h30 / vend 8H-14H', 'inconnue');
-  chef_mission: String[] = new Array<String>('Dayamand Naidoo', 'Boumediene Guennad');*/
+  //variable permettant remplir info_amb
+  pays: String[];
+  ville_localisation: String[];
+  adresse: String[];
+  telephone:String[];
+  fax: String[];
+  email: String[];
+  site_web: String[];
+  heure_ouverture: String[];
+  chef_mission: String[];
+  
+  //information necessaire aux embassades
+  info_amb: Array<{ pays: String, 
+    ville_loc: String, 
+    adresse: String,
+    tel: String, 
+    fax: String, 
+    email: String, 
+    site_web: String,
+    Heure_ouv: String, 
+    chef_mis: String }>;
 
   constructor(public navCtrl: NavController, private storage: Storage, public navParams: NavParams) {
-        
+    this.load();
   }
 
   //initialisation de la liste des amabassades
-  /*initAmbassade()
+  initAmbassade()
   {
+    //initialisation de nosvariables
+    this.pays = ['Afrique du sud', 'Algerie'];
+    this.adresse = ['Cocody President 08 BP 1806 Abidjan 08', 
+     '53 Boulevard clozel 01 BP 1015 Abidjan 01'];
+    this.telephone = ['22 44 59 63', '20 32 32 40'];
+    this.fax = ['22 44 74 50', '20 22 37 12'];
+    this.email = ['abidjan@dirco.gov.za', 'aucun'];
+    this.site_web = ['aucun', 'aucun'];
+    this.heure_ouverture = ['Lundi-jeudi 08H-16h30 / vend 8H-14H', 'inconnue'];
+    this.chef_mission = ['Dayamand Naidoo', 'Boumediene Guennad'];
+    
+    this.info_amb = [];
+    var j : Number = this.pays.length;
+
+   //boucle pour ajouter les info dans la variable info_amb et dans notre base
+    for(let i=0; i <= j ; i++)
+    {
+      
+      for (let a = 0; a<= j; a++){
+      this.ville_localisation = ['Abidjan'];
+      }
+
+      //recuperation des donnees et initialisation de la variable info_amb
+      this.info_amb.push({
+        pays: this.pays[i],
+        ville_loc: this.ville_localisation[i],
+        adresse: this.adresse[i],
+        tel: this.telephone[i],
+        fax: this.fax[i],
+        email: this.email[i],
+        site_web: this.site_web[i],
+        Heure_ouv: this.heure_ouverture[i],
+        chef_mis: this.chef_mission[i]
+
+      });
+
+    
+    }
+      //stockage en bdd
+      this.storage.set('ambassade.ville_localisation', this.ville_localisation);
+      this.storage.set('ambassade', this.info_amb);
+
+    /*
     this.storage.get('amabassade').then((resultat)=>{
       var result = resultat;
       var i = 0;
@@ -83,12 +138,12 @@ export class AmbassadePage {
         result.unshift(this.ambassade);
         this.storage.set('ambassade', result);
     })
-      
+      */
   
-  }*/
+  }
 
   //charger les ambassade presente et les afficher
-  /*load()
+  load()
   {
     this.initAmbassade();
     this.storage.get("ambassade").then((result)=>{
@@ -98,7 +153,7 @@ export class AmbassadePage {
       this.ambassade_bdd = new Array<Ambassade>();
     }
    });
-  }*/
+  }
   
 
 }
